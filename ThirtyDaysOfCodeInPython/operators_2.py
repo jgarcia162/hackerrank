6881d20bb5dd1c6086f8abe572f8ea2f2c8b9223
Jose Garcia <jogarcia809@yahoo.com>
2016-10-27 18:10:07 -0400

    updated operators py

--- ThirtyDaysOfCodeInPython/operators_2.py
+++ ThirtyDaysOfCodeInPython/operators_2.py
@@ -7,20 +7,6 @@ import sys
 # meal price being added as tax) for a meal, find and print the meal's total
 # cost. 
 
-      
-#         // Write your calculation code here.
-#         double totalCost = totalCostOfMeal(mealCost,tipPercent,taxPercent);
-#         // cast the result of the rounding operation to an int and save it as totalCost 
-#         int totalCostRounded = (int) Math.round(totalCost);
-      
-#         // Print your result
-#         System.out.println("The total meal cost is "+String.valueOf(totalCostRounded)+" dollars.");
-#     }
-    
-#     public static double totalCostOfMeal(double baseCost,int tip, int tax){
-# 		return baseCost + ((baseCost*tip)/100) + ((baseCost*tax)/100);
-# 	}
-
 scan = sys.stdin
 mealCost = float(scan.readline())
 tipPercent = int(scan.readline())
